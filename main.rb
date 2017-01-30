a618a859509bb5e1a298b45e7176293bcc2341e9
Bernix01 <gbernal096@gmail.com>
2017-01-29 21:50:18 -0500

    print, value, parenthesized_letter, declarevar

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative 'parser'
 str = 'for a in b:\n\t a = open("jajajaj")'
 str2= "for line in archivo"
-puts str
-a = Parser.parse(str)
+str3= gets
+puts str3
+a = Parser.parse(str3)
 puts a.to_s
