aa9cd407b162a279981dd84e9776944645e8a470
Bernix01 <gbernal096@gmail.com>
2017-02-21 08:27:57 -0500

    menu code indentantion fixes and test file update

--- test_example.py
+++ test_example.py
@@ -1,5 +1,13 @@
-f = open("archivo.txt")
-f=open("otro_archivo.csv","r+")
+f = open("archivo.txt") 
+ f=open("otro_archivo.csv","r+")
 arhivo.write("Hola mundo")
 for linea in archivo:
 	print linea
+f=open("archivo.txt")
+for line in f:
+	sinespacios=line.strip()
+	datos=line.split(",")
+file=open("datos_usuarios.txt","w")
+datos=["datos user1","datos user2"]
+for datos in dato:
+	file.write(dato)
